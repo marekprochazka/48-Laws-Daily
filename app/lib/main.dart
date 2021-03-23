0685be268fabe052e283b56ae2add6f2dd7ec2c6
Marek Prochazka <m.prochazka2002@gmail.com>
2021-03-23 17:35:52 +0100

    run only on first run function

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,37 @@
+import 'dart:ffi';
+
 import 'package:app/router.dart';
 import 'package:app/src/pages/law_detail/law_detail.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Void> initDatabase() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool didStarted = prefs?.getBool("didStarted") ?? false;
+    if (!didStarted) {
+      print("GOING FOR DATABASE INIT");
+      prefs.setBool("didStarted", true);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
